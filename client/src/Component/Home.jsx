8ea0fc83fc847a843b66c2b41374c249bc02768d
IsuruMJA99 <132061327+IsuruMJA99@users.noreply.github.com>
2024-05-02 13:46:34 +0530

    Reapply "add navigation bar"
    This reverts commit 1b619f186d177616d01a7c42f1e312a97ad9bca2.

--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Navigate, Link, useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
 
 function Home () {
   const navigate = useNavigate();
@@ -21,29 +22,33 @@ function Home () {
       });
   }, []);
 
-  const handleLogout = () => {
-    axios.get('http://localhost:3000/auth/logout')
-      .then(res => {
-        if (res.data.status) {
-          navigate('/login');
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  // const handleLogout = () => {
+  //   axios.get('http://localhost:3000/auth/logout')
+  //     .then(res => {
+  //       if (res.data.status) {
+  //         navigate('/login');
+  //       }
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
 
   if (!authenticated) {
     return null; // or loading spinner or message
   }
 
   return (
+<>
+    <Navbar/>
     <div>
       Home<br/>
+      
       <button className="w-full py-2 text-white bg-green-500 rounded-lg cursor-pointer hover:bg-green-700"> <Link to="/dashboard" > Dashboard</Link></button>
       <br/><br/>
-      <button onClick={handleLogout} className="w-full py-2 text-white bg-green-500 rounded-lg cursor-pointer hover:bg-green-700">Logout</button>
+      {/* <button onClick={handleLogout} className="w-full py-2 text-white bg-green-500 rounded-lg cursor-pointer hover:bg-green-700">Logout</button> */}
     </div>
+    </>
   );
 }
 
